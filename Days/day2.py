ce238b809b1e73a6a3019c21bdf695d3355fb7fd
Enzo F. Borges <enzo.alternativo@hotmail.com>
2025-04-22 22:06:30 -0300

    Day 2: Variaveis

---
+++ Days/day2.py
@@ -0,0 +1,18 @@
+nome = 'xxxx'
+idade = 23
+altura = 1.88
+is_student = True
+email = None
+irmaos = ['xxxx', 'yyyy', 'zzzz']
+endereco = {'rua':'x','cidade':'y'}
+
+print('Nome: ',nome)
+print('Idade ',idade)
+print('Altura ',altura)
+print('Estudante: ',is_student)
+print('Email: ',email)
+print('Irmão 1: ',irmaos[0])
+print('Irmão 2: ',irmaos[1])
+print('Irmão 3: ',irmaos[2])
+print('Rua: ',endereco['rua'])
+print('Cidade: ',endereco['cidade'])

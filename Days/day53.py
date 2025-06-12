328bfd1975967063b80bda47778faeb397dc9349
Enzo F. Borges <enzo.alternativo@hotmail.com>
2025-06-12 08:25:11 -0300

    Day 53: Operações de Conjuntos

---
+++ Days/day53.py
@@ -0,0 +1,10 @@
+"""Operações de conjuntos"""
+
+c1 = {1, 2, 3, 4, 5, 6}
+c2 = {4, 5, 6, 7, 8, 9}
+
+uniao = c1 | c2
+interseção = c1 & c2
+adição = c1 + c2
+subtração = c1 - c2
+subSimetrica = c1 ^ c2

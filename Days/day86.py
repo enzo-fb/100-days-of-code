aad0c3751f536c76b80b17b2d081f5e3569590ff
Enzo F. Borges <enzo.alternativo@hotmail.com>
2025-07-15 21:58:29 -0300

    Day 86: Projeto Raspberry Pi

---
+++ Days/day86.py
@@ -0,0 +1,16 @@
+"""Projeto Raspberry Pi"""
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(18, GPIO.OUT)
+
+for i in range(5):
+    GPIO.output(18, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(18, GPIO.LOW)
+    time.sleep(1)
+
+GPIO.cleanup()
